321e2848d3b42dfa3376b0573f7052a47158e268
bhatiagargi8 <86569082+bhatiagargi8@users.noreply.github.com>
2021-06-28 09:24:25 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer>
+      <p> copyright by shape ai @ {new Date().getFullYear()} </p>
+    </footer>
+  );
+}
+export default Footer;
